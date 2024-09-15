673945cd4d899786ed67d9273627c7344f6fba07
edenlisk <nkumbuyenock@gmail.com>
2024-09-15 23:33:35 +0200

    Add graphql plugin

--- config/plugins.js
+++ config/plugins.js
@@ -14,5 +14,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
   // ...
 });
